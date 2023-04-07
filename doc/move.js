023dbf681585024185b7ec01484e52435f3f4217
yanjinke <yanjk@hzzh.com>
2023-04-07 17:39:33 +0800

    flx: github

--- doc/move.js
+++ doc/move.js
@@ -24,24 +24,5 @@ const copyDir = (src, dest) => {
     }
   }
 };
-const emptyDir = (dir) => {
-  // 检查目录是否存在，如果不存在则退出函数
-  if (!fs.existsSync(dir)) return;
-
-  // 获取目录下的所有文件和文件夹
-  const files = fs.readdirSync(dir);
-
-  // 遍历所有文件和文件夹，递归删除文件夹和文件
-  for (let file of files) {
-    const filePath = path.join(dir, file);
-    if (fs.lstatSync(filePath).isDirectory()) {
-      emptyDir(filePath);
-      fs.rmdirSync(filePath);
-    } else {
-      fs.unlinkSync(filePath);
-    }
-  }
-};
-emptyDir("docs");
 // 复制 src 目录下的所有文件和文件夹到 dist 目录下
 copyDir("doc/.vitepress/dist", "docs");
