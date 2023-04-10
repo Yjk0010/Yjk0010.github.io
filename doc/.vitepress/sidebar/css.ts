8c957ade6c97142ec14a8feb44190f22c748bc1b
yanjinke <yanjk@hzzh.com>
2023-04-10 19:38:59 +0800

    feat:  css

--- doc/.vitepress/sidebar/css.ts
+++ doc/.vitepress/sidebar/css.ts
@@ -14,6 +14,15 @@ export default [
       [
         { text: '简单术语', link: '/css/terminology/main' },
         { text: '层叠', link: '/css/terminology/Cascading' },
+        { text: '包含块', link: '/css/terminology/bhk' },
+      ]
+  },
+  {
+    text: '属性',
+    collapsed: false,
+    items:
+      [
+        { text: '属性值', link: '/css/attr/main' },
       ]
   },
 ]
