6cd24962fe6d4a4ffa561914b6b3a034b136bc36
yanjinke <yanjk@hzzh.com>
2023-04-25 13:31:54 +0800

    feat: 删除流程图报错文件

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,7 +3,6 @@ import DefaultTheme from "vitepress/theme";
 
 import Badge from "/components/Badge/index.vue";
 import Flowchart from "/components/Flowchart/index.vue"
-import Mermaid from "/components/Mermaid/index.vue"
 import "./custom.css"
 
 const theme: Theme = {
@@ -11,7 +10,6 @@ const theme: Theme = {
   enhanceApp({ app }) {
     app.component("Badge", Badge);
     app.component("Flowchart", Flowchart);
-    app.component("Mermaid", Mermaid);
   },
 };
 
