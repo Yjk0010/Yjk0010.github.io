42c4b9abc172fcf0b39f94b77a48d7ab738fcc07
yanjinke <yanjk@hzzh.com>
2023-04-25 13:44:03 +0800

    feat: 删除流程图相关 打包报错

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,14 +2,14 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from "vitepress/theme";
 
 import Badge from "/components/Badge/index.vue";
-import Flowchart from "/components/Flowchart/index.vue"
+// import Flowchart from "/components/Flowchart/index.vue"
 import "./custom.css"
 
 const theme: Theme = {
   ...DefaultTheme,
   enhanceApp({ app }) {
     app.component("Badge", Badge);
-    app.component("Flowchart", Flowchart);
+    // app.component("Flowchart", Flowchart);
   },
 };
 
