1557a2886790789516f2ba1da6247f0277db6b8f
yanjinke <yanjk@hzzh.com>
2023-04-25 16:36:41 +0800

    feat: add 浏览器渲染原理

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,12 +3,14 @@ import DefaultTheme from "vitepress/theme";
 
 import Badge from "/components/Badge/index.vue";
 // import Flowchart from "/components/Flowchart/index.vue"
+import Line from "/components/Line/index.vue";
 import "./custom.css"
 
 const theme: Theme = {
   ...DefaultTheme,
   enhanceApp({ app }) {
     app.component("Badge", Badge);
+    app.component("Line", Line);
     // app.component("Flowchart", Flowchart);
   },
 };
