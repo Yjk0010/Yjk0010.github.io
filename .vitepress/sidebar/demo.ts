e63ae4515d6a4e16baa2482c763e59daa30e1b87
yanjinke <yanjk@hzzh.com>
2023-06-06 14:25:04 +0800

    feat: 更新loading

--- .vitepress/sidebar/demo.ts
+++ .vitepress/sidebar/demo.ts
@@ -15,7 +15,7 @@ export default [
         { text: '粒子时钟', link: '/demo/particleClock/main' },
         { text: '复杂动画控制', link: '/demo/complexAnimation/main' },
         { text: '无限视差滚动', link: '/demo/infiniteScroll/main' },
-        { text: '背景随图片变化', link: '/demo/backgroundForImage/main' },
+        { text: '背景随图片变化 以及 loading', link: '/demo/backgroundForImage/main' },
       ]
   },
 ]
