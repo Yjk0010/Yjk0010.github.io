b2afb1a021cc3a984534b10dc1affae1c56900cb
Kingke Yan <yanjinke2008@gmail.com>
2023-06-27 23:03:16 +0800

    fix

--- docs/components.d.ts
+++ docs/components.d.ts
@@ -9,7 +9,10 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    ElBadge: typeof import('element-plus/lib')['ElBadge']
     ElButton: typeof import('element-plus/lib')['ElButton']
+    ElCol: typeof import('element-plus/lib')['ElCol']
+    ElInput: typeof import('element-plus/lib')['ElInput']
+    ElInputNumber: typeof import('element-plus/lib')['ElInputNumber']
+    ElRow: typeof import('element-plus/lib')['ElRow']
   }
 }
