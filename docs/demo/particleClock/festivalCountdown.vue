6cda281dc46877c0cf1f7961489d0408f8067786
yanjinke <yanjk@hzzh.com>
2023-07-07 18:37:19 +0800

    feat: 创建

---
+++ docs/demo/particleClock/festivalCountdown.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="festivalCountdown"></div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, onMounted } from "vue";
+const props = withDefaults(defineProps<{}>(), {});
+const thisYear = new Date().getFullYear();
+onMounted(() => {
+  fetch(
+    `https://www.mxnzp.com/api/holiday/list/year/${thisYear}/festival?app_id=xjrlpumueaxukyov&app_secret=Q2FoMFNub0FndmtJUGRUUElIWTRLQT09`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((res) => {
+      const { code, data } = res || {};
+    });
+});
+</script>
+
+<style lang="scss" scoped>
+.festivalCountdown {
+  display: flex;
+}
+</style>
--- docs/demo/particleClock/main.md
+++ docs/demo/particleClock/main.md
@@ -5,6 +5,7 @@
 <script setup>
 import demo from "./index.vue"
 import card from "./flipCard.vue"
+import festivalCountdown from "./festivalCountdown.vue"
 import {ref} from 'vue'
 const refCount = ref(0)
 const countChange = ()=>{
@@ -15,6 +16,7 @@ const countChange = ()=>{
 <card :total="8" :current="refCount"></card>
 <button @click="countChange">+++</button>
 <demo ></demo>
+<festivalCountdown/>
 
 ::: details 点击展开
 <<< docs/demo/particleClock/index.vue
