2c282d0d948e17118b37357c37256585f49deed2
yanjinke <yanjk@hzzh.com>
2023-12-28 11:28:11 +0800

    feat: lastUpdate

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
   lang: 'zh-CN',
   srcDir: 'docs',
   description: "Some simple notes",
+  lastUpdated: true,
   head: [
     ['link', { rel: "icon", href: "/favicon.ico" }],
   ],
